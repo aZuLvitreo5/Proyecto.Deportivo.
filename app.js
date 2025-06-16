590d15da4bfcd5b6711b719304b1c5117f5aba50
aZuLvitreo5 <di.azulvitreo5israel@yahoo.com.mx>
2025-06-15 19:40:18 -0600

    cambios en imagenes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const slides = [
     image: "images/Imagen.003.png" // Usa la ruta relativa a la carpeta images
   },
   {
-    title: "¿Por qué X?",
+    title: "¡Por qué X?",
     content: "Porque queremos escuchar todas las opiniones, aquí tod@s somos tomad@s en cuenta, queremos establecer contigo los valores, los objetivos y los alcances de este Proyecto, así cómo el nombre del equipo, el color del uniforme y todas esas cosas que van sumando para hacer una gran familia deportiva.",
     image: "images/Imagen.004.png"
   },
